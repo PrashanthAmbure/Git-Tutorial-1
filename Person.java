bb61ae9849d09e1e1fabcfcd98655c9462772fc0
PrashanthAmbure <p.soccer10@gmail.com>
2015-12-14 16:31:05 +0530

    Introduced a pojo for Person.
    Person.java
    
    git add command.
    
    Added comment section.

---
+++ Person.java
@@ -0,0 +1,20 @@
+class Person{
+	/*
+*/
+	private String firstName;
+	private int age;
+
+	Person(String firstName, int age){
+		this.firstName = firstName;
+		this.age = age;
+	}
+
+	public void setFirstName(String firstName){
+		if(null !=firstName || !"".equals(firstName))
+			this.firstName = firstName;
+	}
+
+	public String getFirstName(){
+		return firstName;
+	}
+}
--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 Basic git commands.
 
 >git clone _url_
+>git add .
