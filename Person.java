a18fe3ced67f3ad9f9dfc0d35540a2ec42701fe1
Prashanth Ambure <p.soccer10@gmail.com>
2015-12-14 16:33:37 +0530

    Introduced exception handling.

--- Person.java
+++ Person.java
@@ -6,7 +6,9 @@ class Person{
 		this.firstName = firstName;
 	}
 
-	public void setFirstName(String firstName){
+	public void setFirstName(String firstName) throws IllegalArgumentException{
+		if(StringUtils.isEmpty(firstName))
+			throw new IllegalArgumentException("First name can't be blank.");
 		this.firstName = firstName;
 	}
 
